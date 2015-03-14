69c2bf7438c304a440ad558a7ea52635e8cced13
maximelidolff <maximelidolff@gmail.com>
2015-03-14 13:43:39 +0100

    chimere_maxime_lidolff

---
+++ Chimère_Unity/Assets/CharacterMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMoves : MonoBehaviour {
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+		if (Input.anyKey == true) {
+						anim.SetBool ("IsWalking", true);
+			Debug.Log("IsWalking test");
+				} else {
+			anim.SetBool ("IsWalking", false);;
+				}
+
+	
+	}
+}
